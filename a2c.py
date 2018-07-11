e9ffbb3ad634488f2e20f0844c0de1c4eddd3b8d
Pr1deT <tingzhang@pal-nat186-151-167.itap.purdue.edu>
2018-07-11 14:45:17 -0400

    add a2c.py

---
+++ a2c.py
@@ -0,0 +1,50 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+class ActorCritic(nn.Module):
+    def __init__(self):
+        super(ActorCritic, self).__init__()
+        self.linear1 = nn.Linear(N_INPUTS, 64)
+        self.linear2 = nn.Linear(64, 128)
+        self.linear3 = nn.Linear(128, 64)
+        
+        self.actor = nn.Linear(64, N_ACTIONS)
+        self.critic = nn.Linear(64, 1)
+    
+    # In a PyTorch model, you only have to define the forward pass. PyTorch computes the backwards pass for you!
+    def forward(self, x):
+        x = self.linear1(x)
+        x = F.relu(x)
+        x = self.linear2(x)
+        x = F.relu(x)
+        x = self.linear3(x)
+        x = F.relu(x) 
+        return x
+    
+    # Only the Actor head
+    def get_action_probs(self, x):
+        x = self(x)
+        action_probs = F.softmax(self.actor(x))
+        return action_probs
+    
+    # Only the Critic head
+    def get_state_value(self, x):
+        x = self(x)
+        state_value = self.critic(x)
+        return state_value
+    
+    # Both heads
+    def evaluate_actions(self, x):
+        x = self(x)
+        action_probs = F.softmax(self.actor(x))
+        state_values = self.critic(x)
+        return action_probs, state_values
+
